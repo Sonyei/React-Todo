aca9443bf0d378b9a8980462e2015ae45adeff6f
Sonyei <esonyei@gmail.com>
2020-11-30 16:06:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  state = {
+  constructor(){
+    super()
+  this.state = {
     todos: []
+    }
   };
 
   componentDidMount = () => {
